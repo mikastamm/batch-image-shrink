57bafb62e4184f150b3877ea6afa94fa7b03f11d
Mika Stamm <mika.stamm1@gmail.com>
2018-12-24 12:42:45 +0100

    Initial Commit

---
+++ image-sizer/image-sizer/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace image_sizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPin = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButtonUseX = new System.Windows.Forms.RadioButton();
+            this.radioButtonUseY = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drop Images";
+            // 
+            // btnPin
+            // 
+            this.btnPin.Location = new System.Drawing.Point(443, 12);
+            this.btnPin.Name = "btnPin";
+            this.btnPin.Size = new System.Drawing.Size(92, 23);
+            this.btnPin.TabIndex = 1;
+            this.btnPin.Text = "Pin";
+            this.btnPin.UseVisualStyleBackColor = true;
+            this.btnPin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(185, 56);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(335, 45);
+            this.trackBar1.SmallChange = 5;
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Value = 80;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quality";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.radioButtonUseY);
+            this.panel1.Controls.Add(this.radioButtonUseX);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(3, 239);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(523, 107);
+            this.panel1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 28);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "New Max Res";
+            // 
+            // radioButtonUseX
+            // 
+            this.radioButtonUseX.AutoSize = true;
+            this.radioButtonUseX.Checked = true;
+            this.radioButtonUseX.Location = new System.Drawing.Point(13, 56);
+            this.radioButtonUseX.Name = "radioButtonUseX";
+            this.radioButtonUseX.Size = new System.Drawing.Size(53, 17);
+            this.radioButtonUseX.TabIndex = 6;
+            this.radioButtonUseX.TabStop = true;
+            this.radioButtonUseX.Text = "Width";
+            this.radioButtonUseX.UseVisualStyleBackColor = true;
+            this.radioButtonUseX.CheckedChanged += new System.EventHandler(this.radioButtonUseX_CheckedChanged);
+            // 
+            // radioButtonUseY
+            // 
+            this.radioButtonUseY.AutoSize = true;
+            this.radioButtonUseY.Location = new System.Drawing.Point(77, 56);
+            this.radioButtonUseY.Name = "radioButtonUseY";
+            this.radioButtonUseY.Size = new System.Drawing.Size(56, 17);
+            this.radioButtonUseY.TabIndex = 7;
+            this.radioButtonUseY.Text = "Height";
+            this.radioButtonUseY.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 84);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(127, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Save with new Name";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PNG",
+            "JPG"});
+            this.comboBox1.Location = new System.Drawing.Point(278, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(216, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(191, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "fileFormat";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 356);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnPin);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPin;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButtonUseY;
+        private System.Windows.Forms.RadioButton radioButtonUseX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ image-sizer/image-sizer/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image_sizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            TopMost = true;
+            btnPin.Text = "Unpin";
+
+        }
+
+        bool useX = true;
+        bool saveWithNewName = true;
+        int res = 600;
+        long quality = 80;
+        ImageFormat imageFormat;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TopMost)
+            {
+                btnPin.Text = "Pin";
+                TopMost = false;
+            }
+            else
+            {
+                btnPin.Text = "Unpin";
+                TopMost = true;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            quality = trackBar1.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            res = (int)numericUpDown1.Value;
+        }
+
+        private void radioButtonUseX_CheckedChanged(object sender, EventArgs e)
+        {
+            useX = !useX;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            saveWithNewName = !saveWithNewName;
+        }
+
+        //Dragdrop sttuff
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Html) || e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            
+            foreach(string file in files)
+            {
+                resizeImage(file).Wait();
+            }
+
+
+        }
+
+        private Task resizeImage(string imagepath)
+        {
+            Task t = Task.Factory.StartNew(() =>
+            {
+                Bitmap image = new Bitmap(imagepath);
+
+                if ((useX && res > image.Width) || (!useX && res > image.Height))
+                    return;
+
+                int width = useX ? res : res * image.Width / image.Height;
+                int height = useX ? res * image.Height / image.Width : res;
+
+                Bitmap destImage = new Bitmap(image, new Size(width, height));
+
+                //Rectangle destRect = new Rectangle(0, 0, width, height);
+
+                //using (var graphics = Graphics.FromImage(destImage))
+                //{
+                //    graphics.CompositingMode = CompositingMode.SourceCopy;
+                //    graphics.CompositingQuality = CompositingQuality.HighQuality;
+                //    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                //    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                //    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                //    using (var wrapMode = new ImageAttributes())
+                //    {
+                //        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                //        graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                //    }
+                //}
+
+                //Save as jpg
+                ImageCodecInfo encoder = GetEncoder(imageFormat);
+
+                // Create an Encoder object based on the GUID  
+                // for the Quality parameter category.  
+                System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+
+                // Create an EncoderParameters object.  
+                // An EncoderParameters object has an array of EncoderParameter  
+                // objects. In this case, there is only one  
+                // EncoderParameter object in the array.  
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, quality);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                if (saveWithNewName)
+                    destImage.Save(imagepath + "_resize." + imageFormat.ToString().ToLower(), encoder, myEncoderParameters);
+                else
+                    destImage.Save(imagepath, encoder, myEncoderParameters);
+
+            });
+            return t;
+        }
+
+        ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedText == "PNG")
+            {
+                imageFormat = ImageFormat.Png;
+            }
+            else if (comboBox1.SelectedText == "JPG")
+            {
+                imageFormat = ImageFormat.Jpeg;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            imageFormat = ImageFormat.Png;
+
+        }
+    }
+}
